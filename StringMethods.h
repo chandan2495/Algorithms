b3b214b01f89becdd6f7b1c4b320a9b82d7910b4
chandan2495 <chandan2495@gmail.com>
2016-05-02 23:45:32 +0530

    kmp updated

--- StringMethods.h
+++ StringMethods.h
@@ -19,34 +19,25 @@ int StringMethods:: kmp(string text, string pattern) {
 	tLen = text.length();
 
 	// calculate prefix-suffix of pattern
-	prefixSuffix = (int*)malloc(sizeof(int)*pLen);
-	prefixSuffix[0]=0;
-	for(i=1,j=0;i<pLen;i++) {
-		if(pattern[i]==pattern[j]) {
-			prefixSuffix[i]=j+1;
-			j++;
-		} else {	// if not match reset j to 0
-			j=0;
-			if(pattern[i]==pattern[j]) {
-				prefixSuffix[i]=j+1;				
-				j++;
-			}
-		}		
+	prefixSuffix = (int*)calloc(sizeof(int),(pLen+1));	
+	prefixSuffix[0]=-1;
+	for(i=1,j=-1;i<pLen;) {
+		while(j>=0 && pattern[i]!=pattern[j]) // if not matched go back to find last prefixsuffix match
+				j = prefixSuffix[j];									
+		i++,j++;
+		prefixSuffix[i]=j;	// if match store j+1
 	}
 
 	// start pattern search in main text using prefixSuffix array
 	i=0; j=0;
 	while(i<tLen) {
-		if (text[i+j] == pattern[j]) {
-			j++;
-		} else {
-			i = i + j;
-			j = prefixSuffix[j-1];
-			i = i-j;
-		}
-		if (j==pLen) {
-			printf("Found at index %d\n", i);
-			break;
+		while(j>=0 && text[i]!=pattern[j]) 
+			j = prefixSuffix[j];
+		j++;i++;
+		if (j==pLen)
+		{
+			printf("Found pattern at index %d\n", i-j);
+			j = prefixSuffix[j];	// reset j to find all the patterns in text
 		}
 	}
 }
